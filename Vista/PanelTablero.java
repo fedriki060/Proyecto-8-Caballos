b2156effa590962963a09b70993ac27d24f29c8e
Samuel Acosta <samuel.acosta28@eia.edu.co>
2026-04-24 08:43:43 -0500

    feat: crear clase PanelTablero para representar el tablero

--- Vista/PanelTablero.java
+++ Vista/PanelTablero.java
@@ -1,4 +1,29 @@
 package Vista;
 
-public class PanelTablero {
+import javax.swing.*;
+import java.awt.*;
+import controlador.Controlador;
+
+public class VentanaPrincipal extends JFrame {
+
+    private PanelTablero panelTablero;
+    private JButton btnResolver;
+
+    public VentanaPrincipal() {
+        setTitle("Problema de los 8 Caballos");
+        setSize(600, 650);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        panelTablero = new PanelTablero();
+        add(panelTablero, BorderLayout.CENTER);
+
+        btnResolver = new JButton("Resolver");
+        add(btnResolver, BorderLayout.SOUTH);
+
+        Controlador controlador = new Controlador(panelTablero);
+        btnResolver.addActionListener(controlador);
+
+        setVisible(true);
+    }
 }
