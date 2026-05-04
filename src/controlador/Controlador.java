7b600d52d2d308acb50aaf9fa5c2cd1972e7c263
Samuel Acosta <samuel.acosta28@eia.edu.co>
2026-05-04 18:52:42 -0500

    Feat(vista): se realiza un controlador auxiliar para poder correr la interfaz

--- src/controlador/Controlador.java
+++ src/controlador/Controlador.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+package controlador;
+
+import Vista.PanelTablero;
 
 public class Controlador {
+
+    private PanelTablero panelTablero;
+
+    public Controlador(PanelTablero panelTablero) {
+        this.panelTablero = panelTablero;
+    }
+
+    public boolean resolverVacio() {
+        // Caballos falsos para probar que la interfaz los dibuja
+        int[][] tablero = new int[8][8];
+        tablero[0][0] = 1;
+        tablero[2][1] = 1;
+        tablero[4][2] = 1;
+        tablero[6][3] = 1;
+        tablero[1][4] = 1;
+        tablero[3][5] = 1;
+        tablero[5][6] = 1;
+        tablero[7][7] = 1;
+        panelTablero.setTablero(tablero);
+        return true;
+    }
+
+    public boolean resolverConInicial(int fila, int col) {
+        // Reutiliza el mismo tablero falso
+        return resolverVacio();
+    }
 }
